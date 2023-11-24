45afe8ec58f818779763211fc47a25000a266a7e
juicyldoin <ldoinplay224@gmail.com>
2023-11-24 15:39:27 +0200

    Packet update

--- src/main/java/net/jnetpack/packet/Packet.java
+++ src/main/java/net/jnetpack/packet/Packet.java
@@ -3,7 +3,16 @@ package net.jnetpack.packet;
 import net.jnetpack.packet.interfaces.IReader;
 import net.jnetpack.packet.interfaces.IWriter;
 
+import java.util.PriorityQueue;
+
 /**
  * JNet packet
  */
-public abstract class Packet implements IWriter, IReader {}
+public abstract class Packet implements IWriter, IReader {
+
+    public void work() {}
+
+    public PriorityQueue<Packet> feedback() {
+        return null;
+    }
+}
--- src/main/java/net/jnetpack/packet/PacketPriority.java
+++ src/main/java/net/jnetpack/packet/PacketPriority.java
@@ -1,12 +1,22 @@
 package net.jnetpack.packet;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+
 /**
  * {@link Packet JNet packets} priorities
  */
+@Getter
+@AllArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public enum PacketPriority {
 
-    HIGH,
-    MEDIUM,
-    LOW
+    HIGH((byte) 0),
+    MEDIUM((byte) 1),
+    LOW((byte) 2);
+
+    byte id;
 
 }
